6f90a3dce8f372f045dfc606554b97925ec9260d
BishoySedra <bishosedra0@gmail.com>
2023-04-26 20:14:10 +0200

    2nd upload

---
+++ Project2/Admin.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Course.h"
+//#include "Student.h"
+#include <string>
+#include <vector>
+#include<iostream>
+
+class Admin {
+
+	std::string name;
+	std::string password;
+	std::vector<Course> availableCourses;
+	std::vector<Course> finishedCourses;
+	//std::vector<Student> students;
+
+public:
+
+	Admin(std::string name, std::string password);
+	bool Login(std::string name, std::string password);
+	void addCourse(Course);
+	void viewCourseStudents(Course);
+	//void viewStudentCourses(Student);
+
+
+};
